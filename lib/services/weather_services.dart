a63f3e810fd090587a852688d0a34bf9b421806a
Walaahosny <whussien82@gmail.com>
2023-07-19 18:12:51 +0300

    convert string to url

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,3 +1,9 @@
-class weatherServices{
-  void getWeather({required String cityName}){}
+import 'package:http/http.dart' as http;
+
+class weatherServices {
+  void getWeather({required String cityName}) {
+    Uri url = Uri.parse(
+        'http://api.weatherapi.com/v1/current.json?key=f68f720c766c44bb8b400412231907&q=London&aqi=no');
+    http.get(url);
+  }
 }
